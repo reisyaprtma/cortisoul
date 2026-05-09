27f8a01007de53d729a7f4f9695e77dbd1466b6b
adriyan <adrianadrian.aa613@gmail.com>
2026-05-09 19:30:49 +0700

    feat: menambahkan endpoint authentications

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import users from '../services/users/routes/index.js';
+import authentications from '../services/authentications/routes/index.js';
 
 const routes = Router();
 
 routes.use('/', users);
+routes.use('/', authentications);
 
 export default routes;
