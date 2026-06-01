497d9a743c51258e8344ee2c22d413d5d6fccee9
reisyaprtma <d1041221032@student.untan.ac.id>
2026-06-01 16:23:33 +0700

    favicon update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="id" className="h-full" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/cortisoul-logo.png" />
         {/* Prevent flash of wrong theme: read localStorage before first paint */}
         <script
           dangerouslySetInnerHTML={{
